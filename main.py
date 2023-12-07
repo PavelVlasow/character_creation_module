04a922ad12766e57289d09a0d792b608b5d492bb
Pavel Vlasov <pasha.vlasov.97@mail.ru>
2023-12-07 22:19:33 +0300

    finalcut this project

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class Character():
 
         return f'{self.name} блокировал {value_defence} ед. урона.'
 
-    def spesial(self):
+    def special(self):
 
         return (f'{self.name} применил специальное умение '
                 f'{self.SPECIAL_SKILL} {self.SPECIAL_BUFF}')
@@ -63,6 +63,40 @@ class Healer(Character):
     SPECIAL_SKILL = 'Защита'
 
 
-warrior = Warrior('Кодослав')
-print(warrior)
-print(warrior.attack())
+def choice_char_class(char_name: str) -> Character:
+    game_classes = {
+        'warrior': Warrior,
+        'mage': Mage,
+        'healer': Healer,
+        }
+
+    approve_choice: str = None
+
+    while approve_choice != 'y':
+        selected_class = input('Введи название персонажа, '
+                               'за которого хочешь играть: Воитель — warrior, '
+                               'Маг — mage, Лекарь — healer: ')
+        char_class: Character = game_classes[selected_class](char_name)
+        print(char_class)
+        approve_choice = input('Нажми (Y), чтобы подтвердить выбор, '
+                               'или любую другую кнопку, '
+                               'чтобы выбрать другого персонажа ').lower()
+    return char_class
+
+
+def start_training(character: Character):
+    """
+    Принимает на вход имя и класс персонажа.
+    Возвращает сообщения о результатах цикла тренировки персонажа.
+    """
+    cmd = None
+    commands = {
+        'attack': character.attack,
+        'defence': character.defence,
+        'special': character.special,
+    }
+    while cmd != 'skip':
+        cmd = input('Введи команду: ')
+        if cmd in commands:
+            print(commands[cmd]())
+    return 'Тренировка окончена.'
